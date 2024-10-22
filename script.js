5c192ad4578b646d23b20d48c89b2510fd8663fa
khan450 <khangeorgem@gmail.com>
2024-10-22 12:30:51 -0400

    Added music video page with CSS and JavaScript animation

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿document.getElementById('animateButton').addEventListener('click', function () {
+    var button = document.getElementById('animateButton');
+    button.style.position = 'relative';
+    var position = 0;
+    var interval = setInterval(frame, 10);
+
+    function frame() {
+        if (position == 350) {
+            clearInterval(interval);
+        } else {
+            position++;
+            button.style.left = position + 'px';
+        }
+    }
+});
